01c745293a53b31f6a562a769d21eeb0100237d4
ElimelechEhrlich <ee525566@gmail.com>
2025-11-06 21:24:42 +0200

    Update goblin.py

--- core/goblin.py
+++ core/goblin.py
@@ -15,7 +15,19 @@ class Goblin:
         print (f'I am The terrifying {self.type}!! my name is {self.name}')
 
     def attack(self):
-        return f'{self.type} attack!'
+        if self.speed + rollspeed > player.armor_rating:
+            print (f'{self.name}: injury!')
+            rolldamage = roll_dice(6)
+            if self.weapon == 'knife':
+                damage = self.power + rolldamage*0.5
+            elif self.weapon == 'axe':
+                damage = self.power + rolldamage*1.5
+            elif self.weapon == 'sword':
+                damage = self.power + rolldamage
+            player.hp -= damage
+        else:
+            print (f'{self.name}: miss..')
+            return
     
     def run_away(self):
             return 'run away!' 
