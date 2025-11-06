d3c6f4dc09e64d954162ae05f151dbc5732dc990
Elimelech Ehrlich <ee525566@gmail.com>
2025-11-06 09:15:51 +0200

    readme file

--- core/goblin.py
+++ core/goblin.py
@@ -1,9 +1,3 @@
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.hp = 50
-        self.speed = 8
-        self.power = 9
-        self.rating_armor = 
+
 
         
