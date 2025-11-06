7a3468d6d31c0bd0ccb690c445c086574ad3ff18
ElimelechEhrlich <ee525566@gmail.com>
2025-11-06 21:21:49 +0200

    Refactor attack method for damage calculation
    Enhance attack method to include damage calculation based on weapon type and player armor rating.

--- core/orc.py
+++ core/orc.py
@@ -14,5 +14,17 @@ class Orc:
         print (f'I am The terrifying {self.type}!! my name is {self.name}')
 
     def attack(self):
-        return f'{self.type} attack!'
+        if self.speed + rollspeed > player.armor_rating:
+            print (f'{self.name}: injury!')
+            rolldamage = self.roll_dice(6)
+            if self.weapon == 'knife':
+                damage = self.power + rolldamage*0.5
+            elif self.weapon == 'axe':
+                damage = self.power + rolldamage*1.5
+            elif self.weapon == 'sword':
+                damage = self.power + rolldamage
+            player.hp -= damage
+        else:
+            print (f'{current_player.name}: miss..')
+            return
     
